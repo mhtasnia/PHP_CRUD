e76ce05fca1d6a8f01b34ebc39748268a5d72b5f
Meherose Hossain Tasnia <115734452+mhtasnia@users.noreply.github.com>
2025-04-23 20:09:26 +0600

    Update database.php
    removed the db details

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'root');
-    define('DB_NAME', 'labproject');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
 
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
